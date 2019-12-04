ef0627c38d7c36ee3b6f9f768bb6d85c1c34b05e
RoxBibop <thibault.malik@gmail.com>
2019-12-04 16:54:13 +0100

    formulaire tout fini !

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                         <p>Bienvenue !</p>
                         <p>Plus qu'une étape avant de répondre ou poster une annonce !</p>
                         <p>Vous avez changé d'avis ?</p>
-                        <a href="">Retour à L'accueil</a>
+                        <a href="{{ url('/') }}">Retour à L'accueil</a>
                     </div>    
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                         <path fill="#FFFFFF" fill-opacity="1" d="M0,96L34.3,128C68.6,160,137,224,206,218.7C274.3,213,343,139,411,112C480,85,549,107,617,128C685.7,149,754,171,823,149.3C891.4,128,960,64,1029,80C1097.1,96,1166,192,1234,192C1302.9,192,1371,96,1406,48L1440,0L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="">
-                            <label for="password" class="">{{ __('Password') }}</label>
+                            <label for="password" class="">{{ __('Password') }}</label><br>
                             <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -65,15 +65,14 @@
                         </div>
 
                         <div class="">
-                            <label for="password-confirm" class="">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="">{{ __('Confirm Password') }}</label><br>
                             <input id="password-confirm" type="password" class="" name="password_confirmation" required autocomplete="new-password">
                         </div>
 
-                        <div class="">
+                        <div class="btn">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('Register') }}
-                            </button>
-                                    
+                            </button>   
                         </div>
                     </form>
                 </div>
